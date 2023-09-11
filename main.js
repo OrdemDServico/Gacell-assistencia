cad7aefa353fc6159d720158cc0c91cd9e73668c
LucianoSilva2020 <93166470+LucianoSilva2020@users.noreply.github.com>
2023-09-11 10:56:59 -0300

    gvfjgfv

--- main.js
+++ main.js
@@ -116,3 +116,16 @@ botao.addEventListener('click', function tab() {
     }
 
 })
+
+var msgCookies = document.getElementById('cookies-msg')
+
+function aceito(){
+    localStorage.lgpd = "sim"
+    msgCookies.classList.remove('mostrar')
+}
+
+if(localStorage.lgpd == 'sim'){
+    msgCookies.classList.remove('mostrar')
+}else{
+    msgCookies.classList.add('mostrar')
+}
